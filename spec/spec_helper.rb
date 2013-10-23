2592d8d8fb820824bfd4075c377115407db80888
Jorge Calás Lozano <calas@qvitta.net>
2013-10-23 17:17:39 +0200

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sr/jimson'
+require 'simplecov'
 
 SPEC_URL = 'http://localhost:8999'
+
+SimpleCov.start 'rails' if ENV['COVERAGE']
